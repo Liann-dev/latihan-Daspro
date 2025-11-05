65b466cb0c69fcac430d38f3e79277e67c1a9b8c
Berlianda M Putra <berliandamputra@gmail.com>
2025-11-05 12:41:17 +0700

    Biodata Saya

---
+++ Perkenalan.py
@@ -0,0 +1,5 @@
+print ("Halo! Selamat datang di program Python pertama saya.")
+print ("==Biodata Singkat==")
+nama = "Berlianda Muhammad Putra"
+umur = 18  
+print ("Nama saya " + nama + ", umur saya " + str(umur) + " tahun.")
