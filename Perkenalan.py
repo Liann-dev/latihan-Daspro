6fa193af7cd098156d859a674a21d4c5bac168f8
Berlianda M Putra <berliandamputra@gmail.com>
2025-11-06 21:24:33 +0700

    ulang

--- Perkenalan.py
+++
@@ -1,9 +0,0 @@
-print ("Halo! Selamat datang di program Python pertama saya.")
-print ("==Biodata Singkat==")
-nama = "Berlianda Muhammad Putra"
-umur = 18  
-print ("Nama saya " + nama + ", umur saya " + str(umur) + " tahun.")
-print ("Saya sedang belajar pemrograman Python.")
-print ("Saya berharap dapat membuat banyak program menarik di masa depan!")
-print ("Terima kasih telah menjalankan program ini!")
-print ("Selamat tinggal!")
--- Study Kasus 1.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-# Latihan No 1
-celcius = np.array([0, 10, 20, 30, 40, 50, 60, 70, 80, 90,])
-farenheit = (celcius * 9/5) + 32
-print("Suhu dalam Farenheit:", farenheit)
-print("Suhu dalam Celcius:", celcius)
